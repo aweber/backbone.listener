702ccabd73d99e36ea5127e7cd734225c0bed4e6
Gavin M. Roy <gavinr@aweber.com>
2014-07-09 15:19:21 -0400

    Wrap the scope properly

--- backbone.listener.js
+++ backbone.listener.js
@@ -12,44 +12,57 @@
  * events binding for DOM events, instead binding to the Backbone.Events event
  * stream.
  */
-Backbone.Listener = function(attributes, options) {
-  var attrs = attributes || {};
-  if (options === undefined) options = {};
-  this.cid = _.uniqueId('c');
-  _.extend(this, _.pick(options, listenerOptions));
-  this.bindEvents();
-  this.initialize.apply(this, arguments);
-};
-
-_.extend(Backbone.Listener.prototype, Backbone.Events, {
-
-  // Initialize is an empty function by default. Override it with your own
-  // initialization logic.
-  initialize: function(){},
-
-  // Set callbacks, where `this.events` is a hash of
-  //
-  // *{"event": "callback"}*
-  //
-  //     {
-  //       'eventString'        'callbackMethod',
-  //       'other-event':       function(e) { ... }
-  //     }
-  //
-  // pairs. Callbacks will be bound to the view, with `this` set properly.
-  bindEvents: function(events) {
-    if (!(events || (events = _.result(this, 'events')))) return this;
-    for (var key in events) {
-      var method = events[key];
-      if (!_.isFunction(method)) method = this[events[key]];
-      if (!method) continue;
-      Backbone.on(key, method);
-    }
-    return this;
+(function (factory) {
+  if (typeof exports === 'object') {
+    module.exports = factory(require('underscore'), require('backbone'));
+  } else if (typeof define === 'function' && define.amd) {
+    define(['underscore', 'backbone'], factory);
+  } else {
+    factory(_, Backbone);
   }
+})(function (_, Backbone) {
 
+  "use strict";
+
+  Backbone.Listener = function(attributes, options) {
+    var attrs = attributes || {};
+    if (options === undefined) options = {};
+    this.cid = _.uniqueId('c');
+    _.extend(this, _.pick(options, listenerOptions));
+    this.bindEvents();
+    this.initialize.apply(this, arguments);
+  };
+
+  _.extend(Backbone.Listener.prototype, Backbone.Events, {
+
+    // Initialize is an empty function by default. Override it with your own
+    // initialization logic.
+    initialize: function(){},
+
+    // Set callbacks, where `this.events` is a hash of
+    //
+    // *{"event": "callback"}*
+    //
+    //     {
+    //       'eventString'        'callbackMethod',
+    //       'other-event':       function(e) { ... }
+    //     }
+    //
+    // pairs. Callbacks will be bound to the view, with `this` set properly.
+    bindEvents: function(events) {
+      if (!(events || (events = _.result(this, 'events')))) return this;
+      for (var key in events) {
+        var method = events[key];
+        if (!_.isFunction(method)) method = this[events[key]];
+        if (!method) continue;
+        Backbone.on(key, method);
+      }
+      return this;
+    }
+
+  });
+
+  var listenerOptions = ['model', 'collection', 'id', 'attributes', 'events'];
+
+  Backbone.Listener.extend = Backbone.extend;
 });
-
-var listenerOptions = ['model', 'collection', 'id', 'attributes', 'events'];
-
-Backbone.Listener.extend = Backbone.extend;
